21ee3e4df6364facb79da96b217f3e3a2b9f31e7
Alexsander Chagas Dambros <alexsanderdambros@gmail.com>
2020-06-24 10:27:52 -0300

    Estado e Cidade

--- src/main/java/com/alexdambros/cursospring/CursospringApplication.java
+++ src/main/java/com/alexdambros/cursospring/CursospringApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.alexdambros.cursospring.domain.Categoria;
+import com.alexdambros.cursospring.domain.Cidade;
+import com.alexdambros.cursospring.domain.Estado;
 import com.alexdambros.cursospring.domain.Produto;
 import com.alexdambros.cursospring.repositories.CategoriaRepository;
+import com.alexdambros.cursospring.repositories.CidadeRepository;
+import com.alexdambros.cursospring.repositories.EstadoRepository;
 import com.alexdambros.cursospring.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursospringApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
@@ -27,6 +35,7 @@ public class CursospringApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
+		// Instanciando Categorias e produtos
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
@@ -43,6 +52,21 @@ public class CursospringApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		// Instanciando Estados e Cidades
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
 	}
 	
 	
